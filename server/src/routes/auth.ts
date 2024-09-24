78cbe4a973d81b1c39b3083ba5feaddb50e64a74
Robert Gleason <l2ob122@gmail.com>
2024-09-23 21:14:02 -0700

    Refactor auth route to validate email before generating auth link

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -3,6 +3,20 @@ import { Router } from 'express';
 
 const authRouter = Router();
 
-authRouter.post('/generate-link', generateAuthLink);
+authRouter.post(
+  '/generate-link',
+  (req, res, next) => {
+    const { email } = req.body;
+
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(email)) {
+      // send error
+      return res.status(422).json({ error: 'invalid email' });
+    }
+
+    next();
+  },
+  generateAuthLink,
+);
 
 export default authRouter; // auth routes
