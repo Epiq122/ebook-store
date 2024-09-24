cf586eab6d0d4203ff1c4db44370a17eddc18017
Robert Gleason <l2ob122@gmail.com>
2024-09-23 18:57:42 -0700

    typescript setup

---
+++ server/src/index.ts
@@ -0,0 +1,10 @@
+import { add } from '@/test/math';
+
+const a = 1;
+const b = 2;
+
+// log the sum of a and b to the console
+console.log(a + b);
+
+// log the result of the add function to the console
+console.log('this is from the math.ts' + ' ' + add(a, b));
